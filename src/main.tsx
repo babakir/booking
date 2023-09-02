2f2ea05113964b4dc56f86a718441c93ae6fc242
B A B A K <babakir73@gmail.com>
2023-09-02 22:06:00 +0330

    error handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import App from './App.tsx'
 import './index.css'
+import Root, { loader as rootLoader } from './routes/roots';
+import ErrorPage from './error-page';
+import Contact from './contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
